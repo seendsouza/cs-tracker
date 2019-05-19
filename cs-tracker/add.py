e4a04a452cbb19d9079eac26f9d4009ee7f71b00
Sean D'Souza <sdammobubbles@gmail.com>
2019-05-19 10:48:20 -0400

    feat: add 'add session' functionality
    creates a new session by creating a directory in cs-tracker/data

--- cs-tracker/add.py
+++ cs-tracker/add.py
@@ -3,6 +3,7 @@ add timestamped entries with cs count at 5 minutes for training type
 add new training type
 add new session of entries
 """
+import os
 
 import click
 
@@ -18,6 +19,25 @@ def entry():
 def training():
     pass
 
-@add.command(help='session')
-def session():
-    pass
+@add.command(help='create a new session to store training data')
+@click.option('-v', '--verbose', is_flag = True, help='print a message for each created directory')
+@click.argument('directory')
+def session(directory, verbose):
+    """Creates a new session
+
+    Creates a new session by creating a folder in cs-tracker/data with the user's custom directory name
+
+    Args:
+        directory: name of the directory the session will be stored in
+        verbose: boolean of whether to print Directory Created
+    
+    Raises:
+        FileExistsError: Cannot create a file when that file already exists
+    """
+    try:
+        path = os.path.relpath('../data/{}'.format(directory))
+        os.mkdir(path)
+        if verbose:
+            click.echo("Directory " + directory + " Created")
+    except FileExistsError:
+        click.echo("Cannot create a session when that session already exists: '{}'".format(directory))
