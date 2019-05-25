c2488630747b7e0d9fdb4455900335728eda2bc2
Sean D'Souza <sdammobubbles@gmail.com>
2019-05-25 12:42:06 -0400

    feat: add 'add training' functionality
    creates a new training type by creating a csv file in
    cs-tracker/data/<session_name>

--- cs-tracker/add.py
+++ cs-tracker/add.py
@@ -16,8 +16,42 @@ def entry():
     pass
 
 @add.command(help='training type')
-def training():
-    pass
+@click.option('-v', '--verbose', is_flag = True, help='print a message for each created training type')
+@click.option('-d', '--directory', help='specify the session to put the training type in')
+@click.argument('filename')
+def training(verbose, directory, filename):
+    """Creates a new training type
+
+    Creates a new training type by creating a file in cs-tracker/<session_name> with the user's custom training type name
+
+    Args:
+        directory: name of the directory the session will be stored in
+        filename: name of the directory the session will be stored in
+        verbose: boolean of whether to print Directory Created
+
+    Raises:
+        FileNotFoundError: 
+        FileExistsError: Cannot create a file when that file already exists
+        PermissionError:
+    """
+    try:
+        path = os.path.relpath('../data/{}'.format(directory))
+        if os.path.exists(path) == False or directory == "":
+            raise FileNotFoundError
+        path += "\{}.csv".format(filename)
+        abs_path = os.path.abspath(path)
+        if os.path.exists(abs_path):
+            raise FileExistsError
+        open(abs_path,'w').close()
+        if verbose:
+            click.echo("Training Type " + filename + " Created")
+
+    except FileNotFoundError:
+        click.echo("Cannot create a training type when the session does not exist: '{}'".format(directory))
+    except FileExistsError:
+        click.echo("Cannot create a training type when that training type already exists: '{}'".format(filename))
+    except PermissionError:
+        click.echo("Cannot create a training type when the user does not have the permsissions to write to the directory: '{}'".format(abs_path))
 
 @add.command(help='create a new session to store training data')
 @click.option('-v', '--verbose', is_flag = True, help='print a message for each created directory')
